8d7b1b7a58c029bfedb75eee9420265447daeff2
Javier Prieto <jprieton@gmail.com>
2015-08-24 11:18:30 -0430

    Ajustes Funciones

--- module.php
+++ module.php
@@ -3,7 +3,7 @@
  * Plugin Name: JP Theme Tools Post Module
  * Plugin URI: https://github.com/jprieton/jp-theme-tools-polls-module/
  * Description: Post module for JP Theme Tools
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Javier Prieto
  * Text Domain: jptt
  * Domain Path: /languages
@@ -14,6 +14,52 @@ defined( 'ABSPATH' ) or die( 'No direct script access allowed' );
 
 define( 'JPTT_POST_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'JPTT_POST_PLUGIN_URI', plugin_dir_url( __FILE__ ) );
+
+/**
+ * Indica si el usuario ha votado en un post
+ * @global \wpdb $wpdb
+ * @param int $post_id
+ * @param int $user_id
+ * @return boolean
+ */
+function has_voted( $post_id = NULL, $user_id = NULL ) {
+	global $wpdb;
+	$wpdb instanceof \wpdb;
+
+	$user_id = ((int) $user_id)? : get_current_user_id();
+	$post_id = ((int) $post_id) ? : get_the_ID();
+
+	if ( !$user_id && !$post_id ) {
+		return FALSE;
+	}
+
+	$is_voted = (bool) $wpdb->get_var( "SELECT vote_id FROM {$wpdb->prefix}favorite WHERE post_id = '{$post_id}' AND user_id = '{$user_id}' LIMIT 1" );
+
+	return $is_voted;
+}
+
+/**
+ * Indica si el post esta marcado como favorito por el usuario
+ * @global \wpdb $wpdb
+ * @param int $post_id
+ * @param int $user_id
+ * @return boolean
+ */
+function is_favorite( $post_id = NULL, $user_id = NULL ) {
+	global $wpdb;
+	$wpdb instanceof \wpdb;
+
+	$user_id = ((int) $user_id)? : get_current_user_id();
+	$post_id = ((int) $post_id) ? : get_the_ID();
+
+	if ( !$user_id && !$post_id ) {
+		return FALSE;
+	}
+
+	$is_favorite = (bool) $wpdb->get_var( "SELECT vote_id FROM {$wpdb->prefix}favorite WHERE post_id = '{$post_id}' AND user_id = '{$user_id}' LIMIT 1" );
+
+	return $is_favorite;
+}
 
 register_activation_hook( __FILE__, function() {
 	global $wpdb;
@@ -26,11 +72,21 @@ register_activation_hook( __FILE__, function() {
 					"COLLATE {$wpdb->collate}" :
 					'';
 
-	$query = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}favorite` ("
+	$favorite_query = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}favorite` ("
 					. "`favorite_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,"
 					. "`post_id` bigint(20) unsigned NOT NULL DEFAULT '0',"
 					. "`user_id` bigint(20) DEFAULT NULL,"
 					. "PRIMARY KEY (`favorite_id`),"
+					. "KEY `post_id` (`post_id`)"
+					. ") ENGINE=InnoDB {$charset} {$collate} AUTO_INCREMENT=1";
+	$wpdb->query( $favorite_query );
+
+	$query = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}votes` ("
+					. "`vote_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,"
+					. "`post_id` bigint(20) unsigned NOT NULL DEFAULT '0',"
+					. "`user_id` bigint(20) DEFAULT NULL,"
+					. "`vote_value` bigint(20),"
+					. "PRIMARY KEY (`vote_id`),"
 					. "KEY `post_id` (`post_id`)"
 					. ") ENGINE=InnoDB {$charset} {$collate} AUTO_INCREMENT=1";
 	$wpdb->query( $query );
@@ -47,10 +103,28 @@ add_action( 'wp_ajax_user_post_toggle_favorite', function () {
 	$post_id = (int) $Input->get( 'post_id' );
 	$user_id = (int) $userdata->ID;
 
-	$is_favorite = (bool) $wpdb->get_var( "SELECT favorite_id FROM {$wpdb->prefix}favorite WHERE post_id = '{$post_id}' AND user_id = '{$user_id}' LIMIT 1" );
-
-	if ( !$is_favorite ) {
+	if ( !is_favorite( $post_id, $user_id ) ) {
 		$wpdb->insert( "{$wpdb->prefix}favorite", compact( 'post_id', 'user_id' ) );
+		wp_send_json( TRUE );
+	} else {
+		wp_send_json( FALSE );
+	}
+}, 10 );
+
+add_action( 'wp_ajax_user_post_vote', function () {
+
+	global $wpdb;
+	$wpdb instanceof \wpdb;
+
+	$Input = new \jptt\core\Input();
+	$userdata = get_userdata( get_current_user_id() );
+
+	$post_id = (int) $Input->get( 'post_id' );
+	$user_id = (int) $userdata->ID;
+	$vote_value = (int) $Input->get( 'post_id' );
+
+	if ( !has_voted( $post_id, $user_id ) ) {
+		$wpdb->insert( "{$wpdb->prefix}voted", compact( 'post_id', 'user_id', 'vote_value' ) );
 		wp_send_json( TRUE );
 	} else {
 		$wpdb->delete( "{$wpdb->prefix}favorite", compact( 'post_id', 'user_id' ) );
@@ -90,9 +164,9 @@ add_action( 'wp_footer', function() {
 	if ( !is_singular() ) return FALSE;
 	?>
 	<script>
-	  $(function () {
-	      jQuery.get('<?php echo admin_url( 'admin-ajax.php' ) ?>', {post_id: 1000, action: 'user_post_visited'});
-	  });
+		$(function () {
+				jQuery.get('<?php echo admin_url( 'admin-ajax.php' ) ?>', {post_id: 1000, action: 'user_post_visited'});
+		});
 	</script>
 	<?php
 } );
